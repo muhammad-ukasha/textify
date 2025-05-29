de89aecce1e933451327b43dcda3e5d39e134254
Muhammad Ukasha <49838872+ukasha01@users.noreply.github.com>
2025-05-29 08:39:25 +0500

    chand raat

--- api/axiosInstance.js
+++ api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.0.179:3000/api/v1", // Replace with your backend base URL
+  baseURL: "https://3125ccb2-54a5-4d27-9aef-532ef1bdaccc-00-226tm68i8l9bh.sisko.replit.dev/api/v1", // Replace with your backend base URL
   //   timeout: 10000, // Timeout after 10 seconds
   headers: {
     "Content-Type": "application/json",
